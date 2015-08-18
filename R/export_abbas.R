b79dd653bc21fc549f7aa4ff2915491231941bb7
yusriy <yusriyp@gmail.com>
2015-08-18 19:13:32 +0800

    Edit export script

--- R/export_abbas.R
+++ R/export_abbas.R
@@ -2,6 +2,6 @@
 
 s18temp <- s18[,c(2,14:28,31,32)]
 usmtemp <- usm[,c(2,14:28,31,32)]
-write.table(s18temp,'data/s18.csv',sep=',')
-write.table(usmtemp,'data/usm.csv',sep=',')
+write.table(s18temp,'data/s18.csv',sep=',',row.names=FALSE)
+write.table(usmtemp,'data/usm.csv',sep=',',row.names=FALSE)
 rm(s18temp,usmtemp)
